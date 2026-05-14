8c6c661fa9d1b25db425c8bbc5017fa3bd62c483
Ragavi2909 <rrgavi03@gmail.com>
2026-05-14 14:51:29 +0530

    second ques

---
+++ Prob2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Prob2 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextInt();
+        
+        String rev = "";
+        for(int i=str.length()-1;i>=0;i--){
+            rev += str[i];
+        }
+
+        if(str.equals(rev)){
+            System.out.print("True");
+        }else{
+            System.out.print("False");
+        }
+    }
+}
