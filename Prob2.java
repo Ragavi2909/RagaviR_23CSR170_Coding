ea0f04881d94b10cc706fc7a5641b9e542f4dd58
Ragavi2909 <rrgavi03@gmail.com>
2026-05-14 15:05:13 +0530

    updated

--- Prob2.java
+++ Prob2.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Prob2 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextInt();
+        String str = sc.nextLine();
         
         String rev = "";
         for(int i=str.length()-1;i>=0;i--){
