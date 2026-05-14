b5715b281c44cd045470c9c520573e45df093d7e
Ragavi2909 <rrgavi03@gmail.com>
2026-05-14 14:45:37 +0530

    first commit

---
+++ prob1.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Prob1 {
+    public  static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[arr.length];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        int secLarg = arr[0];
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]< max && arr[i]>secLarg){
+                secLarg = arr[i];
+            }
+        }
+        System.out.print(secLarg);
+    }
+    
+}
