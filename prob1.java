5019feec9935d2987a44d0a9d7eedfe3a39dc50f
Ragavi2909 <rrgavi03@gmail.com>
2026-05-14 15:23:35 +0530

    Update prob1.java

--- prob1.java
+++ prob1.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class Prob1 {
     public  static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[arr.length];
+        int[] arr = new int[1000];
         for(int i=0;i<arr.length;i++){
             arr[i] = sc.nextInt();
         }
