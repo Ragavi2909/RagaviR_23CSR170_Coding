95cd4893416bf9df6652577a82fdd6106360688c
Ragavi2909 <rrgavi03@gmail.com>
2026-05-14 15:28:37 +0530

    Update prob4.java

--- prob4.java
+++ prob4.java
@@ -2,7 +2,7 @@ import java.util.*;
 public class prob4 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int[] arr = new int[arr.length];
+        int[] arr = new int[1000];
         for(int i=0;i<arr.length;i++){
             arr[i] = sc.nextInt();
         }
@@ -15,6 +15,6 @@ public class prob4 {
                 }
             }
         }
-        return int[] {-1,-1};
+        return int[];
     }
 }
